ed4bf1edc59d477d2b5ab222601f119950a46163
a.borblyk <borblyk@gmail.com>
2024-03-31 11:12:45 +0200

    Created simple calculator that adds and subtracts two numbers.

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print("Initial commit")
+import sys
+
+
+def add_two_numbers(a, b):
+    return x + y
+
+
+def subtract_two_numbers(a, b):
+    return x - y
+
+
+x = float(sys.argv[1])
+y = float(sys.argv[2])
+
+print(add_two_numbers(x, y))
+print(subtract_two_numbers(x, y))
